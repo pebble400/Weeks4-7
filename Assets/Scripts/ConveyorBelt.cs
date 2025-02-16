11b877a00e1ef4f39b756c3987c405cf10f42860
DESKTOP-KA9TBRQ\Owner <anitahosseini37@gmail.com>
2025-02-16 15:57:54 -0500

    Got the conveyor belt to work. Food will now move along the conveyor belt and be destroyed upon reaching the "serve" plate.

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour
 {
+    public float speed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+
+        if(screenPos.x < 0)
+        {
+            Vector3 fixedPos = new Vector3 (0, 0 , 0);
+            pos.x = Camera.main.WorldToScreenPoint(fixedPos).x;
+            speed = speed * -1;
+        }
+        if(screenPos.x > Screen.width)
+        {
+            Vector3 fixedPos = new Vector3 (Screen.width, 0 , 0);
+            pos.x = Camera.main.WorldToScreenPoint(fixedPos).x;
+            speed = speed * -1;
+        }
+        transform.position = pos;
     }
 }
